a678e642bbe2ecce1d7a2b97d2f73b5cb2af2cd1
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-25 16:48:02 +0300

    Add project files.

---
+++ CSharp14/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Bir metin giriniz...");
+string cumle = Console.ReadLine();
+bool deg = cumle.Contains("a");
+
+if (deg)
+{
+    string cumle2 = cumle.Replace("a", "z");
+    Console.WriteLine(cumle2);
+}
+else
+{
+    string cumle3 = cumle.ToUpper();
+    Console.WriteLine(cumle3);
+}
+
